6bba1f67bd183d5d02d045baddcf6686e4c6e1ac
obify <76449271+obify@users.noreply.github.com>
2024-11-24 20:34:47 +0100

    Update SecurityConfig.java

--- src/main/java/com/mycompany/generic/config/SecurityConfig.java
+++ src/main/java/com/mycompany/generic/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(req ->
                         req.requestMatchers("/api/v1/noauth/**").permitAll()
                                 .anyRequest().authenticated()
